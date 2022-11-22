bff3bc94b2627044ead58d216e1e179ab50e797a
Leonardo <leonardo.trejos@utp.edu.co>
2022-11-22 12:33:40 -0500

    inicio y registro usuarios

---
+++ src/app/services/auth.interceptor.ts
@@ -0,0 +1,32 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { Injectable } from '@angular/core'
+import { Observable } from "rxjs";
+import { LoginService } from "./login.service";
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor{
+
+    constructor(private loginService:LoginService){
+
+    }
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        let authoReq = req;
+        const token = this.loginService.getToken();
+        if(token != null){
+            authoReq = authoReq.clone({
+                setHeaders : {Authorization:`Bearer ${token}`}
+            })
+        }
+        return next.handle(authoReq);
+    }
+    
+}
+
+export const authInterceptorProviders=[
+    {
+        provide: HTTP_INTERCEPTORS,
+        useClass: AuthInterceptor,
+        multi:true
+    }
+]
